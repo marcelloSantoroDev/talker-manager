8ace264999aa1c38c013d2d572c2ab8914bec4f2
Marcello <marcelloaltermundi@gmail.com>
2023-02-06 16:20:42 -0300

    feat: add requisito 04

--- src/index.js
+++ src/index.js
@@ -33,6 +33,25 @@ const tokenGenerator = () => {
   return crypto.randomBytes(8).toString('hex');
 };
 
+const validations = (req, res, next) => {
+  const { email, password } = req.body;
+  const regex = /\S+@\S+\.\S+/;
+  if(!email) {
+    return res.status(400).send({ "message": "O campo \"email\" é obrigatório" })
+  } else if (!password) {
+    return res.status(400).send({ "message": "O campo \"password\" é obrigatório" })
+  }
+  const checkPassword = password.length >= 6;
+  const checkEmail = regex.test(email);
+  if(!checkEmail) {
+    return res.status(400).send({ "message": "O \"email\" deve ter o formato \"email@email.com\"" })
+  } else if (!checkPassword) {
+    return res.status(400).send({ "message": "O \"password\" deve ter pelo menos 6 caracteres" })
+  } else {
+    next()
+  }
+}
+
 app.get('/talker/:id', async (req, res) => {
   const talkerId = Number(req.params.id);
   const talkerList = await readFile();
@@ -53,11 +72,7 @@ app.get('/talker', async (req, res) => {
   }
 });
 
-app.post('/login', async(req, res) => {
-  const { email, password } = req.body;
-  const validate = email && password;
-  if(validate){
+app.post('/login', validations, async(req, res) => {
     const token = tokenGenerator();
     return res.status(200).send({ "token": token });
-  }
 })
